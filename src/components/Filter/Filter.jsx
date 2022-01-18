461124f2c5ff452964255058ff0cb4d499d556f5
Christian Lara <christianramirezlara23@gmail.com>
2022-01-18 10:58:13 -0600

    Add toggle theme to the filter options component

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,9 +1,12 @@
 import './Filter.scss';
 import arrowDownIcon from './chevron-down-outline.svg';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import ThemeContext from '../../context/themeContext';
 
 const Filter = () => {
   const [optionsDisplay, setOptionsDisplay] = useState(false);
+  const themeCtx = useContext(ThemeContext);
+  const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
 
   const toggleOptions = () => {
     setOptionsDisplay((lastState) => !lastState);
@@ -11,16 +14,21 @@ const Filter = () => {
 
   return (
     <div className="filter">
-      <div onClick={toggleOptions} className="filter__button">
-        <p className="filter__button__text">Filter by Region</p>
+      <div
+        onClick={toggleOptions}
+        className={`filter__button--${currentTheme}`}
+      >
+        <p className={`filter__button__text--${currentTheme}`}>
+          Filter by Region
+        </p>
         <img
-          className="filter__button__icon"
+          className={`filter__button__icon--${currentTheme}`}
           alt="Arrow icon"
           src={arrowDownIcon}
         />
       </div>
       {optionsDisplay && (
-        <div className="filter__options">
+        <div className={`filter__options--${currentTheme}`}>
           <p>Africa</p>
           <p>America</p>
           <p>Asia</p>
