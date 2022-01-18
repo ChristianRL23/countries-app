8d0cf86dbd8fb67fa7938384dbe741c867271cda
Christian Lara <christianramirezlara23@gmail.com>
2022-01-18 13:02:38 -0600

    Add toggle theme to the main

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
 import OptionsBar from '../../components/OptionsBar/OptionsBar';
+import ThemeContext from '../../context/themeContext';
 import './Main.scss';
 
 const Main = () => {
+  const themeCtx = useContext(ThemeContext);
+  const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
+
   return (
-    <main className="main">
+    <main className={`main--${currentTheme}`}>
       <OptionsBar />
     </main>
   );
