061ef91892bcdeaddc692f8b2933c399fbc5c728
Christian Lara <christianramirezlara23@gmail.com>
2022-01-18 10:42:46 -0600

    Add toggle theme to search bar

--- src/components/OptionsBar/OptionsBar.jsx
+++ src/components/OptionsBar/OptionsBar.jsx
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
+import ThemeContext from '../../context/themeContext';
 import Filter from '../Filter/Filter';
 import './OptionsBar.scss';
 import searchIcon from './search-outline.svg';
 
 const OptionsBar = () => {
+  const themeCtx = useContext(ThemeContext);
+  const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
+
   return (
     <form className="options-bar">
-      <div className="options-bar__search">
-        <img src={searchIcon} className="search__icon" alt="Search icon" />
+      <div className={`options-bar__search--${currentTheme}`}>
+        <img
+          src={searchIcon}
+          className={`search__icon--${currentTheme}`}
+          alt="Search icon"
+        />
         <input
-          className="search__input"
+          className={`search__input--${currentTheme}`}
           placeholder="Search for a country..."
         />
       </div>
