beb13100adf3ce52e4024fa37c01a5b547e1c3bf
Christian Lara <christianramirezlara23@gmail.com>
2022-01-18 20:01:45 -0600

    Style and add theme changer to the Card component

--- src/components/CountryCard/CountryCard.jsx
+++ src/components/CountryCard/CountryCard.jsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import './CountryCard.scss';
+import ThemeContext from '../../context/themeContext';
 
 const CountryCard = ({
   countryName,
@@ -7,20 +9,36 @@ const CountryCard = ({
   countryRegion,
   countryCapital,
 }) => {
+  const themeCtx = useContext(ThemeContext);
+  const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
+
   return (
-    <div className="country-card">
-      <img src={countryFlagImg} alt="Country flag" />
-      <div>
-        <h5>{countryName}</h5>
-        <div>
-          <h6>
-            Population: <span>{countryPopulation}</span>
+    <div className={`country-card--${currentTheme}`}>
+      <img
+        className="country-card__flag"
+        src={countryFlagImg}
+        alt="Country flag"
+      />
+      <div className="country-card__info">
+        <h5 className={`country-card__name--${currentTheme}`}>{countryName}</h5>
+        <div className="country-card__data">
+          <h6 className={`country-card__population--${currentTheme}`}>
+            Population:{' '}
+            <span className={`country-card__population-value--${currentTheme}`}>
+              {countryPopulation}
+            </span>
           </h6>
-          <h6>
-            Region: <span>{countryRegion}</span>
+          <h6 className={`country-card__region--${currentTheme}`}>
+            Region:{' '}
+            <span className={`country-card__region-value--${currentTheme}`}>
+              {countryRegion}
+            </span>
           </h6>
-          <h6>
-            Capital: <span>{countryCapital}</span>
+          <h6 className={`country-card__capital--${currentTheme}`}>
+            Capital:{' '}
+            <span className={`country-card__capital-value--${currentTheme}`}>
+              {countryCapital}
+            </span>
           </h6>
         </div>
       </div>
