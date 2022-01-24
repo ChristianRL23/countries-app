86cb0fe0d54cdc1ef011ab72701dee2a039dfd07
Christian Lara <christianramirezlara23@gmail.com>
2022-01-24 14:57:05 -0600

    Create country component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,12 +6,24 @@ const Button = ({ icon, textContent }) => {
   const themeCtx = useContext(ThemeContext);
   const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
 
-  return (
-    <div className={`button--${currentTheme}`}>
-      <img className={`button__icon--${currentTheme}`} src={icon} alt="Arrow" />
-      <p className={`button__text--${currentTheme}`}>{textContent}</p>
-    </div>
-  );
+  if (icon) {
+    return (
+      <div className={`button--${currentTheme}`}>
+        <img
+          className={`button__icon--${currentTheme}`}
+          src={icon}
+          alt="Arrow"
+        />
+        <p className={`button__text--${currentTheme}`}>{textContent}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`button--text--${currentTheme}`}>
+        <p className={`button__text--${currentTheme}`}>{textContent}</p>
+      </div>
+    );
+  }
 };
 
 export default Button;
--- src/layouts/Country/Country.jsx
+++ src/layouts/Country/Country.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Button from '../../components/Button/Button';
 import ThemeContext from '../../context/themeContext';
 import './Country.scss';
@@ -7,51 +7,92 @@ import ArrowIcon from './arrow-back.svg';
 const Country = () => {
   const themeCtx = useContext(ThemeContext);
   const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    const fetchCountriesData = async () => {
+      const response = await fetch('https://restcountries.com/v2/name/mexico');
+      const data = await response.json();
+      setCountry(data);
+    };
+
+    fetchCountriesData();
+  }, []);
 
   return (
-    <section className={`country--${currentTheme}`}>
-      <div className="country__back-button">
-        <Button icon={ArrowIcon} textContent="Back" />
-      </div>
-      <div className="country__details">
-        <div className="country__details__flag"></div>
-        <div className="country__details__info">
-          <h2 className="country__details__info__name">Country Name</h2>
-          <div className="country__details__info__data">
-            <h6 className="country__details__info__data__detail">
-              Native Name: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Population: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Region: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Sub Region: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Capital: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Top Level Domain: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Currencies: <span className="detail__value">Value</span>
-            </h6>
-            <h6 className="country__details__info__data__detail">
-              Languages: <span className="detail__value">Value</span>
-            </h6>
-          </div>
-          <div className="country__details__info__neighbors">
-            <h6 className="country__details__info__data__detail">Languages:</h6>
-            <Button />
-            <Button />
-            <Button />
+    country.length > 0 && (
+      <section className={`country--${currentTheme}`}>
+        <div className="country__back-button">
+          <Button icon={ArrowIcon} textContent="Back" />
+        </div>
+        <div className="country__details">
+          <img
+            className="country__details__flag"
+            src={country[0].flags.svg}
+            alt="Country flag"
+          />
+          <div className="country__details__info">
+            <h2 className={`country__details__info__name--${currentTheme}`}>
+              {country[0].name}
+            </h2>
+            <div className="country__details__info__data">
+              <div className="details--left">
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Native Name:{' '}
+                  <span className="detail__value">{country[0].nativeName}</span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Population:{' '}
+                  <span className="detail__value">{country[0].population}</span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Region:{' '}
+                  <span className="detail__value">{country[0].region}</span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Sub Region:{' '}
+                  <span className="detail__value">{country[0].subregion}</span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Capital:{' '}
+                  <span className="detail__value">{country[0].capital}</span>
+                </h6>
+              </div>
+              <div className="details--right">
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Top Level Domain:{' '}
+                  <span className="detail__value">{country[0].region}</span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Demonym:{' '}
+                  <span className="detail__value">{country[0].demonym}</span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Currencies:{' '}
+                  <span className="detail__value">
+                    {country[0].currencies[0].name}
+                  </span>
+                </h6>
+                <h6 className={`detail__name--${currentTheme}`}>
+                  Languages:{' '}
+                  <span className="detail__value">
+                    {country[0].languages[0].name}
+                  </span>
+                </h6>
+              </div>
+            </div>
+            <div className="country__details__info__neighbors">
+              <h6 className={`detail__name--${currentTheme}`}>
+                Border Countries:
+              </h6>
+              <Button textContent="Mexico" />
+              <Button textContent="Mexico" />
+              <Button textContent="Mexico" />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    )
   );
 };
 
