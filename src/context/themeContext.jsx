d01fabd9c83b95a5e4dd690d91fecba4ca9a61ce
Christian Lara <christianramirezlara23@gmail.com>
2022-01-24 15:56:32 -0600

    Change body background color when a country is selected

--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -8,7 +8,12 @@ const ThemeContext = React.createContext({
 export const ThemeContextProvider = ({ children }) => {
   const [darkTheme, setDarkTheme] = useState(false);
 
-  const changeThemeHandler = () => setDarkTheme(!darkTheme);
+  const changeThemeHandler = () => {
+    setDarkTheme((lastState) => !lastState);
+    const backgroundColor =
+      darkTheme === true ? 'hsl(0, 0%, 98%)' : 'hsl(207, 26%, 17%)';
+    document.body.style.backgroundColor = backgroundColor;
+  };
 
   return (
     <ThemeContext.Provider value={{ darkTheme, changeThemeHandler }}>
