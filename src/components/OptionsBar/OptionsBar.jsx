377e9685d6960d9504e3ca78e9e07fa069555025
Christian Lara <christianramirezlara23@gmail.com>
2022-01-30 10:31:55 -0600

    Implement error handling when a conutry is not found

--- src/components/OptionsBar/OptionsBar.jsx
+++ src/components/OptionsBar/OptionsBar.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CountriesContext from '../../context/countriesContext';
 import ThemeContext from '../../context/themeContext';
 import Filter from '../Filter/Filter';
@@ -10,6 +10,14 @@ const OptionsBar = () => {
   const currentTheme = themeCtx.darkTheme ? 'dark' : 'light';
   const [searchInput, setSearchInput] = useState('');
   const countriesCtx = useContext(CountriesContext);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setError(null);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [error]);
 
   const searchBarTypingHandler = (e) => {
     setSearchInput(e.target.value);
@@ -17,28 +25,39 @@ const OptionsBar = () => {
 
   const searchCountry = async (e) => {
     e.preventDefault();
-    const response = await fetch(
-      `https://restcountries.com/v2/name/${searchInput}`
-    );
-    const data = await response.json();
-    countriesCtx.setCountrySelected(data[0].name);
-    setSearchInput('');
+    setError(null);
+    try {
+      const response = await fetch(
+        `https://restcountries.com/v2/name/${searchInput}`
+      );
+      const data = await response.json();
+      if (data.status === 404) {
+        throw new Error('Country not found!');
+      }
+      countriesCtx.setCountrySelected(data[0].name);
+      setSearchInput('');
+    } catch (err) {
+      setError(err.message);
+    }
   };
 
   return (
     <form onSubmit={searchCountry} className="options-bar">
-      <div className={`options-bar__search--${currentTheme}`}>
-        <img
-          src={searchIcon}
-          className={`search__icon--${currentTheme}`}
-          alt="Search icon"
-        />
-        <input
-          onChange={searchBarTypingHandler}
-          value={searchInput}
-          className={`search__input--${currentTheme}`}
-          placeholder="Search for a country..."
-        />
+      <div className="options-bar__search__container">
+        <div className={`options-bar__search--${currentTheme}`}>
+          <img
+            src={searchIcon}
+            className={`search__icon--${currentTheme}`}
+            alt="Search icon"
+          />
+          <input
+            onChange={searchBarTypingHandler}
+            value={searchInput}
+            className={`search__input--${currentTheme}`}
+            placeholder="Search for a country..."
+          />
+        </div>
+        {error && <p>{error}</p>}
       </div>
       <Filter />
     </form>
